610489768fbeeb6bbf9fa8b3aa83f2ad3ae5ac64
VivekD-123 <dongarevivek81@gmail.com>
2025-07-18 15:05:40 +0530

    feat: create Razorpay payment service

---
+++ discovery-service/src/test/java/com/devsoncall/streammymeal/discoveryserver/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsoncall.streammymeal.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ payment-service/src/main/java/com/devsoncall/streammymeal/payment/service/PaymentService.java
@@ -0,0 +1,5 @@
+package com.devsoncall.streammymeal.payment.service;
+
+public interface PaymentService {
+    String createRazorpayOrder(int amount, String currency, String receipt);
+} 
